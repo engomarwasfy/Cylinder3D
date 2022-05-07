76ed27ac56ba3e15b14c824ef667ea9cadac7223
liamkboyle <liam.boyle@student.ethz.ch>
2022-05-07 19:11:45 +0200

    Update configs for training on heap data

--- dataloader/pc_dataset.py
+++ dataloader/pc_dataset.py
@@ -483,7 +483,8 @@ def get_heap_label_name(label_mapping):
         heapyaml = yaml.safe_load(stream)
     heap_label_name = dict()
     for i in sorted(list(heapyaml['learning_map'].keys()))[::-1]:
-        val_ = heapyaml['learning_map'][i]
-        heap_label_name[val_] = heapyaml['labels_16'][val_]
+        # val_ = heapyaml['learning_map'][i]
+        # heap_label_name[val_] = heapyaml['labels'][val_]
+        heap_label_name[heapyaml['learning_map'][i]] = heapyaml['labels'][i]
 
     return heap_label_name
