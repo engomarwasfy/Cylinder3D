34b4cf8e3e0384e317959f1acd84723befb54836
liamkboyle <boylel@student.ethz.ch>
2022-05-27 12:21:49 +0200

    Add utils to measure compute time for each step of training

--- train_cylinder_asym_heap.py
+++ train_cylinder_asym_heap.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 import wandb
 
 from prettytable import PrettyTable
-from utils.metric_util import per_class_iu, fast_hist_crop
+from utils.metric_util import per_class_iu, fast_hist_crop, AverageMeter, ProgressMeter
 from dataloader.pc_dataset import get_heap_label_name
 from builder import data_builder, model_builder, loss_builder
 from config.config import load_config_data
@@ -20,21 +20,24 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-use_wandb = True
+use_wandb = False
 if use_wandb:
     wandb.login(key='4d8dd62b978bbed4276d53f03a9e5f4973fc320b')
     run = wandb.init(project="Cylinder3D-Heap", entity="rsl-lidar-seg")
 
 
 def count_parameters(model):
-    table = PrettyTable(["Modules", "Parameters"])
+    table = PrettyTable(["Modules", "Parameters", "Gradients"])
     total_params = 0
     num_layers = 0
     for name, parameter in model.named_parameters():
         if not parameter.requires_grad: continue
-        params = parameter.numel()
-        table.add_row([name, params])
-        total_params+=params
+        params = parameter.size()
+        grads = None
+        if parameter.grad is not None:
+            grads = parameter.grad.size()
+        table.add_row([name, params, grads])
+        # total_params+=params
         num_layers+=1
     print(table)
     print(f"Total Trainable Params: {total_params}")
@@ -44,6 +47,7 @@ def count_parameters(model):
 
 def main(args):
     pytorch_device = torch.device('cuda:0')
+    # torch.cuda.empty_cache()
 
     config_path = args.config_path
 
@@ -73,6 +77,7 @@ def main(args):
     my_model = model_builder.build(model_config)
     if use_wandb:
         run.watch(my_model)
+    my_model.to(pytorch_device)
     optimizer = optim.Adam(my_model.parameters(), lr=train_hypers["learning_rate"])
     if os.path.exists(model_load_path):
         model_dict = torch.load(model_load_path)
@@ -80,7 +85,7 @@ def main(args):
         optimizer.load_state_dict(model_dict['optimizer_state_dict'])
 
     # count_parameters(my_model)
-    my_model.to(pytorch_device)
+    # my_model.to(pytorch_device)
     # optimizer = optim.Adam(my_model.parameters(), lr=train_hypers["learning_rate"])
 
     loss_func, lovasz_softmax = loss_builder.build(wce=True, lovasz=True,
@@ -99,12 +104,30 @@ def main(args):
     check_iter = train_hypers['eval_every_n_steps']
 
     while epoch < train_hypers['max_num_epochs']:
+        count_parameters(my_model)
         loss_list = []
-        pbar = tqdm(total=len(train_dataset_loader))
+        # pbar = tqdm(total=len(train_dataset_loader))
         # time.sleep(1)
+        batch_time = AverageMeter('Time', ':6.3f')
+        data_time = AverageMeter('Data', ':6.3f')
+        totaltime = AverageMeter('Total Time', ':6.3f')
+        dataconversion_time = AverageMeter('Data conversion Time', ':6.3f')
+        forwardtime = AverageMeter('Forward Time', ':6.3f')
+        losstime = AverageMeter('Loss Time', ':6.3f')
+        backward_time = AverageMeter('Backward Time', ':6.3f')
+        optimizer_time = AverageMeter('Optimizer Time', ':6.3f')
+        rest_time = AverageMeter('Rest Time', ':6.3f')
+        progress = ProgressMeter(
+            len(train_dataset_loader),
+            [batch_time, data_time, dataconversion_time, forwardtime, losstime, backward_time, optimizer_time, rest_time],
+            prefix="Epoch: [{}]".format(epoch))
         print("\n Epoch: ", epoch)
+        torch.cuda.synchronize()
+        end = time.time()
         for i_iter, (_, train_vox_label, train_grid, _, train_pt_fea) in enumerate(train_dataset_loader):
-            if global_iter % check_iter == 0 and epoch >= 0:
+            torch.cuda.synchronize()
+            data_time.update(time.time() - end)
+            if global_iter % check_iter == 0 and epoch > 0:
                 my_model.eval()
                 hist_list = []
                 val_loss_list = []
@@ -161,17 +184,29 @@ def main(args):
                       (np.mean(val_loss_list)))
                 if use_wandb:
                     run.log({'Validation loss': np.mean(val_loss_list)})
-
+            torch.cuda.synchronize()
+            t0 = time.time()
             train_pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(pytorch_device) for i in train_pt_fea]
             train_vox_ten = [torch.from_numpy(i).to(pytorch_device) for i in train_grid]
             point_label_tensor = train_vox_label.type(torch.LongTensor).to(pytorch_device)
+            torch.cuda.synchronize()
+            t1 = time.time()
+
 
             # forward + backward + optimize
             outputs = my_model(train_pt_fea_ten, train_vox_ten, train_batch_size)
+            torch.cuda.synchronize()
+            t2 = time.time()
             loss = lovasz_softmax(torch.nn.functional.softmax(outputs), point_label_tensor, ignore=0) + loss_func(
                 outputs, point_label_tensor)
+            torch.cuda.synchronize()
+            t3 = time.time()
             loss.backward()
+            torch.cuda.synchronize()
+            t4 = time.time()
             optimizer.step()
+            torch.cuda.synchronize()
+            t5 = time.time()
             loss_list.append(loss.item())
             if global_iter % 10 == 0 and use_wandb:
                 if len(loss_list) > 0:
@@ -185,7 +220,7 @@ def main(args):
                     print('loss error')
 
             optimizer.zero_grad()
-            pbar.update(1)
+            # pbar.update(1)
             global_iter += 1
             if global_iter % check_iter == 0:
                 if len(loss_list) > 0:
@@ -193,7 +228,35 @@ def main(args):
                           (epoch, i_iter, np.mean(loss_list)))
                 else:
                     print('loss error')
-        pbar.close()
+            torch.cuda.synchronize()
+            t6 = time.time()
+            torch.cuda.synchronize()
+            batch_time.update(time.time() - end)
+            torch.cuda.synchronize()
+            end = time.time()
+
+            # dataloading_time = t1 -t0
+            # forward_time = t2 - t1
+            # loss_time = t3 -t2
+            # backward_time = t4 -t3
+            # optimizer_time = t5 -t4
+            # rest_time = t6 - t5
+            dataconversion_time.update(t1 - t0)
+            forwardtime.update(t2 - t1)
+            losstime.update(t3 - t2)
+            backward_time.update(t4 - t3)
+            optimizer_time.update(t5 - t4)
+            rest_time.update(t6 - t5)
+            # print('\n Dataloading [%]: ', dataloading_time/totaltime *100)
+            # print('\n Forward pass [%]: ', forward_time/totaltime *100)
+            # print('\n Loss calc [%]: ', loss_time/totaltime *100)
+            # print('\n Backward pass [%]: ', backward_time/totaltime *100)
+            # print('\n Optimizer [%]: ', optimizer_time/totaltime *100)
+            # print('\n Rest [%]: ', rest_time/totaltime *100)
+            # print('\n total time [s]: ', totaltime)
+            if global_iter % 10 == 0:
+                progress.display(global_iter)
+        # pbar.close()
         epoch += 1
 
 
